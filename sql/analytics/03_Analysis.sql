8488c72ba063c49d17da999b9ccee0736c397467
Daniel <156577565+Daniel1221342@users.noreply.github.com>
2026-04-21 13:32:47 +0200

    Update and rename newfile to 03_Analysis.sql

---
+++ sql/analytics/03_Analysis.sql
@@ -0,0 +1,7 @@
+
+1.
+
+Select YEAR(Z.Data_Zamowienia) AS Year, MONTH(z.Data_Zamowienia) AS Month, COUNT(*) AS Quantity_Of_Orders
+From Zamowienia AS Z 
+Group by YEAR(Z.Data_Zamowienia), MONTH(z.Data_zamowienia)
+Order by Month, Year
